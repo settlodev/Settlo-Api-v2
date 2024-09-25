680d405305508ea5fc5aabdba5f2531102001325
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2024-09-25 14:31:00 +0300

    Mofify discounts

--- src/main/java/co/tz/settlo/api/discount/DiscountRepository.java
+++ src/main/java/co/tz/settlo/api/discount/DiscountRepository.java
@@ -3,12 +3,17 @@ package co.tz.settlo.api.discount;
 import co.tz.settlo.api.category.Category;
 import co.tz.settlo.api.customer.Customer;
 import co.tz.settlo.api.department.Department;
+import co.tz.settlo.api.expense.Expense;
 import co.tz.settlo.api.location.Location;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
-public interface DiscountRepository extends JpaRepository<Discount, UUID> {
+public interface  DiscountRepository  extends JpaRepository<Discount, UUID>, JpaSpecificationExecutor<Discount> {
+    List<Discount> findAllByLocationId(UUID locationId);
 
     Discount findFirstByDepartment(Department department);
 
