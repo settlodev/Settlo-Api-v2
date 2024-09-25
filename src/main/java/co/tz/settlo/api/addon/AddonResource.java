46b609a69c5a8c4b0413326dec9922acb59cfede
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2024-09-25 10:07:13 +0300

    Modified addons

--- src/main/java/co/tz/settlo/api/addon/AddonResource.java
+++ src/main/java/co/tz/settlo/api/addon/AddonResource.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +18,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.tz.settlo.api.util.RestApiFilter.FieldType;
+import co.tz.settlo.api.util.RestApiFilter.FilterRequest;
+import co.tz.settlo.api.util.RestApiFilter.Operator;
+import co.tz.settlo.api.util.RestApiFilter.SearchRequest;
+
 
 @RestController
-@RequestMapping(value = "/api/addons", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/addons/{locationId}", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AddonResource {
 
     private final AddonService addonService;
@@ -27,25 +34,40 @@ public class AddonResource {
         this.addonService = addonService;
     }
 
+    
+//    @PostMapping
+//    public Page<AddonDTO> searchAddons(@PathVariable UUID locationId, @RequestBody SearchRequest request) {
+//        // Enforce Location filter
+//        FilterRequest locationFilter = new FilterRequest();
+//        locationFilter.setKey("location");
+//        locationFilter.setOperator(Operator.EQUAL);
+//        locationFilter.setFieldType(FieldType.STRING);
+//        locationFilter.setValue(locationId);
+//
+//        request.getFilters().add(locationFilter);
+//
+//        return addonService.searchAll(request);
+//    }
+
     @GetMapping
-    public ResponseEntity<List<AddonDTO>> getAllAddons() {
+    public ResponseEntity<List<AddonDTO>> getAllAddons(@PathVariable UUID locationId) {
         return ResponseEntity.ok(addonService.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AddonDTO> getAddon(@PathVariable(name = "id") final UUID id) {
+    public ResponseEntity<AddonDTO> getAddon(@PathVariable UUID locationId, @PathVariable(name = "id") final UUID id) {
         return ResponseEntity.ok(addonService.get(id));
     }
 
     @PostMapping
     @ApiResponse(responseCode = "201")
-    public ResponseEntity<UUID> createAddon(@RequestBody @Valid final AddonDTO addonDTO) {
+    public ResponseEntity<UUID> createAddon(@PathVariable UUID locationId, @RequestBody @Valid final AddonDTO addonDTO) {
         final UUID createdId = addonService.create(addonDTO);
         return new ResponseEntity<>(createdId, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UUID> updateAddon(@PathVariable(name = "id") final UUID id,
+    public ResponseEntity<UUID> updateAddon(@PathVariable UUID locationId, @PathVariable(name = "id") final UUID id,
             @RequestBody @Valid final AddonDTO addonDTO) {
         addonService.update(id, addonDTO);
         return ResponseEntity.ok(id);
@@ -53,7 +75,7 @@ public class AddonResource {
 
     @DeleteMapping("/{id}")
     @ApiResponse(responseCode = "204")
-    public ResponseEntity<Void> deleteAddon(@PathVariable(name = "id") final UUID id) {
+    public ResponseEntity<Void> deleteAddon(@PathVariable UUID locationId, @PathVariable(name = "id") final UUID id) {
         addonService.delete(id);
         return ResponseEntity.noContent().build();
     }
