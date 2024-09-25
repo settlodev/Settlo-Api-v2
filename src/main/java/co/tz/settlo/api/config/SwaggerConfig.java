9d060900d454b74edb28008cc5243de2cff544a0
Walter Peter Karanja <walter.peter@settlo.co.tz>
2024-09-25 12:49:06 +0300

    Update swagger config
    Add more details to the swagger config file

--- src/main/java/co/tz/settlo/api/config/SwaggerConfig.java
+++ src/main/java/co/tz/settlo/api/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package co.tz.settlo.api.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.IntegerSchema;
@@ -20,20 +21,26 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openApiSpec() {
-        return new OpenAPI().components(new Components()
-                .addSchemas("ApiErrorResponse", new ObjectSchema()
-                        .addProperty("status", new IntegerSchema())
-                        .addProperty("code", new StringSchema())
-                        .addProperty("message", new StringSchema())
-                        .addProperty("fieldErrors", new ArraySchema().items(
-                                new Schema<ArraySchema>().$ref("ApiFieldError"))))
-                .addSchemas("ApiFieldError", new ObjectSchema()
-                        .addProperty("code", new StringSchema())
-                        .addProperty("message", new StringSchema())
-                        .addProperty("property", new StringSchema())
-                        .addProperty("rejectedValue", new ObjectSchema())
-                        .addProperty("path", new StringSchema())));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Settlo API")
+                        .version("1.0.0")
+                        .description("This documentation is for the Settlo API."))
+                .components(new Components()
+                        .addSchemas("ApiErrorResponse", new ObjectSchema()
+                                .addProperty("status", new IntegerSchema())
+                                .addProperty("code", new StringSchema())
+                                .addProperty("message", new StringSchema())
+                                .addProperty("fieldErrors", new ArraySchema().items(
+                                        new Schema<ArraySchema>().$ref("ApiFieldError"))))
+                        .addSchemas("ApiFieldError", new ObjectSchema()
+                                .addProperty("code", new StringSchema())
+                                .addProperty("message", new StringSchema())
+                                .addProperty("property", new StringSchema())
+                                .addProperty("rejectedValue", new ObjectSchema())
+                                .addProperty("path", new StringSchema())));
     }
+
 
     @Bean
     public OperationCustomizer operationCustomizer() {
