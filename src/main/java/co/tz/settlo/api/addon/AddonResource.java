eb2ccbf50ab392a754f472eb49d4dae3c395aa06
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2024-09-26 08:56:45 +0300

    Add API docs for addons

--- src/main/java/co/tz/settlo/api/addon/AddonResource.java
+++ src/main/java/co/tz/settlo/api/addon/AddonResource.java
@@ -1,6 +1,7 @@
 package co.tz.settlo.api.addon;
 
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
@@ -49,17 +50,20 @@ public class AddonResource {
     }
 
     @GetMapping
+    @Tag(name = "Get all Addons", description = "Get all addons under the supplied location ID")
     public ResponseEntity<List<AddonDTO>> getAllAddons(@PathVariable UUID locationId) {
         return ResponseEntity.ok(addonService.findAll(locationId));
     }
 
     @GetMapping("/{id}")
+    @Tag(name = "Get Addon", description = "Get a specific addon by supplying it's ID and location ID")
     public ResponseEntity<AddonDTO> getAddon(@PathVariable UUID locationId, @PathVariable(name = "id") final UUID id) {
         return ResponseEntity.ok(addonService.get(id));
     }
 
     @PostMapping("/create")
     @ApiResponse(responseCode = "201")
+    @Tag(name = "Create Addon", description = "Create an Addon by providing it's JSON as a HTTP body")
     public ResponseEntity<UUID> createAddon(@PathVariable UUID locationId, @RequestBody @Valid final AddonDTO addonDTO) {
         addonDTO.setLocation(locationId);
         final UUID createdId = addonService.create(addonDTO);
@@ -67,6 +71,7 @@ public class AddonResource {
     }
 
     @PutMapping("/{id}")
+    @Tag(name = "Update Addon", description = "Update an Addon by providing the new one as a JSON HTTP body")
     public ResponseEntity<UUID> updateAddon(@PathVariable UUID locationId, @PathVariable(name = "id") final UUID id,
             @RequestBody @Valid final AddonDTO addonDTO) {
         addonDTO.setLocation(locationId);
@@ -76,6 +81,7 @@ public class AddonResource {
 
     @DeleteMapping("/{id}")
     @ApiResponse(responseCode = "204")
+    @Tag(name = "Delete Addon", description = "Delete an Addon by providing it's ID")
     public ResponseEntity<Void> deleteAddon(@PathVariable UUID locationId, @PathVariable(name = "id") final UUID id) {
         addonService.delete(id);
         return ResponseEntity.noContent().build();
