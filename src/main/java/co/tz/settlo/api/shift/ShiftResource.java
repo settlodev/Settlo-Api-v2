0329b0f478f6cb13c4297a03d533cb67303660b2
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2024-09-26 13:28:09 +0300

    Add shift api docs

--- src/main/java/co/tz/settlo/api/shift/ShiftResource.java
+++ src/main/java/co/tz/settlo/api/shift/ShiftResource.java
@@ -2,7 +2,9 @@ package co.tz.settlo.api.shift;
 
 import co.tz.settlo.api.util.ReferencedException;
 import co.tz.settlo.api.util.ReferencedWarning;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/shifts", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Shift Endpoints")
 public class ShiftResource {
 
     private final ShiftService shiftService;
@@ -30,23 +33,27 @@ public class ShiftResource {
     }
 
     @GetMapping
+    @Operation(summary = "Get all Shifts")
     public ResponseEntity<List<ShiftDTO>> getAllShifts() {
         return ResponseEntity.ok(shiftService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a Shift")
     public ResponseEntity<ShiftDTO> getShift(@PathVariable(name = "id") final UUID id) {
         return ResponseEntity.ok(shiftService.get(id));
     }
 
     @PostMapping
     @ApiResponse(responseCode = "201")
+    @Operation(summary = "Create a Shift")
     public ResponseEntity<UUID> createShift(@RequestBody @Valid final ShiftDTO shiftDTO) {
         final UUID createdId = shiftService.create(shiftDTO);
         return new ResponseEntity<>(createdId, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a Shift")
     public ResponseEntity<UUID> updateShift(@PathVariable(name = "id") final UUID id,
             @RequestBody @Valid final ShiftDTO shiftDTO) {
         shiftService.update(id, shiftDTO);
@@ -55,6 +62,7 @@ public class ShiftResource {
 
     @DeleteMapping("/{id}")
     @ApiResponse(responseCode = "204")
+    @Operation(summary = "Delete a Shift")
     public ResponseEntity<Void> deleteShift(@PathVariable(name = "id") final UUID id) {
         final ReferencedWarning referencedWarning = shiftService.getReferencedWarning(id);
         if (referencedWarning != null) {
