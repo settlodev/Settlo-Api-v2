9485808f983b473903d9f2820717e015169ade3a
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2024-09-26 13:46:28 +0300

    Add unit api docs

--- src/main/java/co/tz/settlo/api/unit/UnitResource.java
+++ src/main/java/co/tz/settlo/api/unit/UnitResource.java
@@ -2,7 +2,9 @@ package co.tz.settlo.api.unit;
 
 import co.tz.settlo.api.util.ReferencedException;
 import co.tz.settlo.api.util.ReferencedWarning;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/units", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Unit Endpoints")
 public class UnitResource {
 
     private final UnitService unitService;
@@ -30,23 +33,27 @@ public class UnitResource {
     }
 
     @GetMapping
+    @Operation(summary = "Get all Units")
     public ResponseEntity<List<UnitDTO>> getAllUnits() {
         return ResponseEntity.ok(unitService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a Unit")
     public ResponseEntity<UnitDTO> getUnit(@PathVariable(name = "id") final UUID id) {
         return ResponseEntity.ok(unitService.get(id));
     }
 
     @PostMapping
     @ApiResponse(responseCode = "201")
+    @Operation(summary = "Create a Unit")
     public ResponseEntity<UUID> createUnit(@RequestBody @Valid final UnitDTO unitDTO) {
         final UUID createdId = unitService.create(unitDTO);
         return new ResponseEntity<>(createdId, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a Unit")
     public ResponseEntity<UUID> updateUnit(@PathVariable(name = "id") final UUID id,
             @RequestBody @Valid final UnitDTO unitDTO) {
         unitService.update(id, unitDTO);
@@ -55,6 +62,7 @@ public class UnitResource {
 
     @DeleteMapping("/{id}")
     @ApiResponse(responseCode = "204")
+    @Operation(summary = "Delete a Unit")
     public ResponseEntity<Void> deleteUnit(@PathVariable(name = "id") final UUID id) {
         final ReferencedWarning referencedWarning = unitService.getReferencedWarning(id);
         if (referencedWarning != null) {
