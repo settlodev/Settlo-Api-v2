7a331680002ef412cd4024fb09251f4dd528447d
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2024-09-27 13:48:29 +0300

    Update filtering

--- src/main/java/co/tz/settlo/api/addon/AddonResource.java
+++ src/main/java/co/tz/settlo/api/addon/AddonResource.java
@@ -42,9 +42,9 @@ public class AddonResource {
     public Page<AddonDTO> searchAddons(@PathVariable UUID locationId, @RequestBody SearchRequest request) {
         // Enforce Location filter
         FilterRequest locationFilter = new FilterRequest();
-        locationFilter.setKey("location");
+        locationFilter.setKey("location.id");
         locationFilter.setOperator(Operator.EQUAL);
-        locationFilter.setFieldType(FieldType.STRING);
+        locationFilter.setFieldType(FieldType.UUID_STRING);
         locationFilter.setValue(locationId);
 
         request.getFilters().add(locationFilter);
--- src/main/java/co/tz/settlo/api/supplier/SupplierResource.java
+++ src/main/java/co/tz/settlo/api/supplier/SupplierResource.java
@@ -44,9 +44,9 @@ public class SupplierResource {
     public Page<SupplierDTO> searchSuppliers(@PathVariable UUID locationId, @RequestBody SearchRequest request) {
         // Enforce Location filter
         FilterRequest locationFilter = new FilterRequest();
-        locationFilter.setKey("location");
+        locationFilter.setKey("location.id");
         locationFilter.setOperator(Operator.EQUAL);
-        locationFilter.setFieldType(FieldType.STRING);
+        locationFilter.setFieldType(FieldType.UUID_STRING);
         locationFilter.setValue(locationId);
 
         request.getFilters().add(locationFilter);
