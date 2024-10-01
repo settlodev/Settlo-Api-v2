6cf6e5bab95ffbea8b71a7b341f1add5647ab1ba
Walter Peter Karanja <walter.peter@settlo.co.tz>
2024-10-01 12:14:47 +0300

    Business service improvement

--- src/main/java/co/tz/settlo/api/business/BusinessCreateDTO.java
+++ src/main/java/co/tz/settlo/api/business/BusinessCreateDTO.java
@@ -1,6 +1,5 @@
 package co.tz.settlo.api.business;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
