c0baf4593e94de690d8dc8762a4658f1b8739e34
Maarifa Maarifa <maarifamaarifa20@gmail.com>
2024-10-04 09:05:06 +0300

    Update CSV exceptions comments

--- src/main/java/co/tz/settlo/api/product_csv_parser/exceptions/MissingColumnException.java
+++ src/main/java/co/tz/settlo/api/product_csv_parser/exceptions/MissingColumnException.java
@@ -3,6 +3,7 @@ package co.tz.settlo.api.product_csv_parser.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/// An exception that is thrown when the csv file is missing a required column
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class MissingColumnException extends RuntimeException {
     public MissingColumnException(String columnName) {
